f7dc78d8a1b1a81e8e65d70a0e001569f4528d30
DaeheeKim-Dan <daeheekim7978@gmail.com>
2021-12-27 01:27:12 +0900

    Program terminating key upgrade Print out format upgrade By Dan Kim @2012-12-27, 01:27

--- bom.py
+++ bom.py
@@ -1,12 +1,3 @@
-'''
-Solidworks slddrw 작업시
-풍선번호 배열 이후, BOM 테이블 조작할 때, 숨겨야할 번호를 빨리 파악하기 위해
-나열된 풍선번호를 모두 입력하면,
-정렬된 set를 반환시켜, 나머지 숫자들만 숨겨 작업을 빠르게 진행하도록 하는 프로그램
-Programmed by Dan Kim @2021-12-26
-'''
-
-
 def get_number():
     i = 1
     bom = []
@@ -20,12 +11,15 @@ def get_number():
 
 
 def print_out(result):
-    print("-"*50)
+    print("*"*120)
     print(f"\nInput number of data: {result['input']}")
     print(f"\nNumber set: {result['bom']}, Total {result['set_element']} elements exist.")
+    print("\n")
+    print("*"*120)
+    print("\n"*5)
 
     
-if __name__ == "__main__"    :
+if __name__ == "__main__" :
 
     bom = get_number()
     data_number = len(bom)
@@ -35,6 +29,8 @@ if __name__ == "__main__"    :
     result = {"input":data_number, "bom":bom_set, "set_element":set_number}
     print_out(result)
     
+
+
 
 
         
